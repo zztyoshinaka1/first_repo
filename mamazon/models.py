db2e8b9a40c574cf440d8084bef0e9acd260046b
yoshinaka <yoshinaka@heisei-intelligence.com>
2021-02-19 15:52:39 +0900

    lesson04

--- mamazon/models.py
+++ mamazon/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnails/')
+
+    def __str__(self):
+        return self.name
