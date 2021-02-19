b425e1986fa72a78f91336f0c85f9090da769e01
yoshinaka <yoshinaka@heisei-intelligence.com>
2021-02-19 15:33:45 +0900

    lesson03

--- mamazon/models.py
+++ mamazon/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.ImageField(upload_to='thumbnails/')
