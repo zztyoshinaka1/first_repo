fde5ff60d6487ae890a9d643e778a781a7c723bc
yoshinaka <yoshinaka@heisei-intelligence.com>
2021-02-24 11:21:49 +0900

    lesson07

---
+++ cart/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.conf import settings
+from mamazon.models import Product
+
+User = settings.AUTH_USER_MODEL
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,null=True,blank=True, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mamazon.apps.MamazonConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
